6632ab9ecf89ba5c825a5ccfaaa322067d076d72
arafatsikder <arfatsikder929@gmail.com>
2024-03-19 10:41:32 -0700

    logic injected

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 var setRating;
 
+
+
 function setRating(num){
     setRating = num;
     // console.log(num);
@@ -10,9 +12,12 @@ function setRating(num){
 
 
 
+
+
 function submit(){
     const rating = setRating;
-    const oldDiv = document.getElementById('main');
+    if(rating>0){
+        const oldDiv = document.getElementById('main');
     oldDiv.innerHTML = `
     <div class="main">
         <div class="welcome-img">
@@ -33,6 +38,30 @@ function submit(){
         </div>
     </div>
     `
+    }
+    else{
+    const oldDiv = document.getElementById('main');
+    oldDiv.innerHTML = `
+    <div class="main">
+        <div class="welcome-img">
+            <img src="images/illustration-thank-you.svg" alt="" height="100px" width="150px">
+        </div>
+        <div id="star-number">
+            Please select a number out of 5
+        </div>
+        <div class="thanks-text">
+            <div>
+                <p>
+                    Please let us know your experience. If you are happy in our service please press in 5!
+                </p>
+            </div>
+            <div>
+                    <h2>Thank You!</h2>
+            </div>
+        </div>
+    </div>
+    `
+    }
     
     
 }
